39b6f6ea05a23c365f06702cd3c286aa3449b861
Ib Green <ib@foursquare.com>
2024-07-12 15:14:28 -0400

    fix(dggs-s2): Replace long module with bigint (#6)

--- modules/dggs-s2/src/s2geometry/s2-cell-utils.ts
+++ modules/dggs-s2/src/s2geometry/s2-cell-utils.ts
@@ -2,9 +2,8 @@
 
 import type {S2Cell} from './s2-geometry';
 import {fromHilbertQuadKey, toHilbertQuadkey} from './s2-geometry';
-import Long from 'long';
 
-export function getS2Cell(s2Index: Long): S2Cell {
+export function getS2Cell(s2Index: bigint): S2Cell {
   const key = toHilbertQuadkey(s2Index);
   const s2cell = fromHilbertQuadKey(key);
   return s2cell;
