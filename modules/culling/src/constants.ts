361fa49ed0d3ba7bbc573b51ace91acd96f57cab
Ib Green <ib@foursquare.com>
2024-08-03 08:53:02 -0400

    wip

--- modules/culling/src/constants.ts
+++ modules/culling/src/constants.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) vis.gl contributors
 
-\export const INTERSECTION = {
+export const INTERSECTION = {
   OUTSIDE: -1, // Represents that an object is not contained within the frustum.
   INTERSECTING: 0, // Represents that an object intersects one of the frustum's planes.
   INSIDE: 1 // Represents that an object is fully within the frustum.
