ebb60491850bc4496f8a858627c9afe58395f49a
Ib Green <ib@foursquare.com>
2024-08-03 08:53:25 -0400

    chore: Clean up types (#14)

--- modules/types/src/is-array.ts
+++ modules/types/src/is-array.ts
@@ -2,25 +2,34 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) vis.gl contributors
 
-import {TypedArray, NumericArray} from './array-types';
+import {TypedArray, NumericArray, NumberArray} from './array-types';
 
 /**
  * Check is an array is a typed array
  * @param value value to be tested
- * @returns input as TypedArray, or null
+ * @returns input with type narrowed to TypedArray, or null
  */
 export function isTypedArray(value: unknown): value is TypedArray {
   return ArrayBuffer.isView(value) && !(value instanceof DataView);
 }
 
 /**
- * Check is an array is a numeric array (typed array or array of numbers)
+ * Check is an array is an array of numbers)
  * @param value value to be tested
- * @returns input as NumericArray, or null
+ * @returns input with type narrowed to NumberArray, or null
  */
-export function isNumericArray(value: unknown): value is NumericArray {
+export function isNumberArray(value: unknown): value is NumberArray {
   if (Array.isArray(value)) {
     return value.length === 0 || typeof value[0] === 'number';
   }
-  return isTypedArray(value);
+  return false;
+}
+
+/**
+ * Check is an array is a numeric array (typed array or array of numbers)
+ * @param value value to be tested
+ * @returns input with type narrowed to NumericArray, or null
+ */
+export function isNumericArray(value: unknown): value is NumericArray {
+  return isTypedArray(value) || isNumberArray(value);
 }
