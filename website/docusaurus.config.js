bac91d155ae3e885b2c7d705fc6b588a906dfc39
Xiaoji Chen <cxiaoji@gmail.com>
2024-08-27 14:37:25 -0700

    alpha.10

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -18,20 +18,6 @@ const config = getDocusaurusConfig({
     resolve: {
       alias: {
         'website-examples': resolve('../examples'),
-
-        '@math.gl/types': resolve('../modules/types/src'),
-        '@math.gl/core': resolve('../modules/core/src'),
-        '@math.gl/culling': resolve('../modules/culling/src'),
-        '@math.gl/geospatial': resolve('../modules/geospatial/src'),
-        '@math.gl/geoid': resolve('../modules/geoid/src'),
-        '@math.gl/polygon': resolve('../modules/polygon/src'),
-        '@math.gl/proj4': resolve('../modules/proj4/src'),
-        '@math.gl/web-mercator': resolve('../modules/web-mercator/src'),
-        '@math.gl/sun': resolve('../modules/sun/src'),
-        '@math.gl/ddgs-geohash': resolve('../modules/ddgs-geohash/src'),
-        '@math.gl/dggs-quadkey': resolve('../modules/dggs-quadkey/src'),
-        '@math.gl/dggs-s2': resolve('../modules/dggs-s2/src'),
-
         '@probe.gl/bench': resolve('../node_modules/@probe.gl/bench')
       }
     }
