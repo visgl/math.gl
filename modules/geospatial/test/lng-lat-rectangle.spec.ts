5e08ff24f427b57daa223f9c641dffa72cbf769f
Raimund Schnürer <raimund.schnurer@epfl.ch>
2025-01-14 14:30:41 +0100

    removed in favor of geo-rectangle

--- modules/geospatial/test/lng-lat-rectangle.spec.ts
+++
@@ -1,7 +0,0 @@
-import test from 'tape-promise/tape';
-import {LngLatRectangle} from '@math.gl/geospatial';
-
-test('LngLatRectangle', (t) => {
-  t.ok(new LngLatRectangle(0, 0, 0, 0));
-  t.end();
-});
