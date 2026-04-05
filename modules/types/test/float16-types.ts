3c95a24175eb0bf5c3337675e840ecf73fe68213
Ib Green <ib@unfolded.ai>
2026-04-05 10:35:22 -0400

    prettier

--- modules/types/test/float16-types.ts
+++ modules/types/test/float16-types.ts
@@ -4,10 +4,10 @@
 
 import type {NumericArray, TypedArray, TypedArrayConstructor} from '@math.gl/types';
 
-type GlobalFloat16ArrayConstructor =
-  typeof globalThis extends {Float16Array: infer T} ? T : never;
-type GlobalFloat16Array =
-  typeof globalThis extends {Float16Array: {prototype: infer T}} ? T : never;
+type GlobalFloat16ArrayConstructor = typeof globalThis extends {Float16Array: infer T} ? T : never;
+type GlobalFloat16Array = typeof globalThis extends {Float16Array: {prototype: infer T}}
+  ? T
+  : never;
 
 declare const float16Array: GlobalFloat16Array;
 declare const float16ArrayConstructor: GlobalFloat16ArrayConstructor;
